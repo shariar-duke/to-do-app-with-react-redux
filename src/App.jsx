557434ea10b683ed17a44ac75434f0dcae57e05d
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-08 13:32:51 +0600

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Navbar from "./components/Navbar"
+import Taskboard from "./components/Taskboard"
 export default function App() {
   return (
-    <div>
+    <div className="bg-[#F0FFF0]">
       <Navbar/>
+      <div className="min-h-[90vh] flex justify-center items-center">
+        <Taskboard/>
+      </div>
     </div>
   )
 }
