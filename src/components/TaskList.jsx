0dc11bf0830cc6d585d6980107f02c208b18baca
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-08 13:33:32 +0600

    TaskList.jsx

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,9 @@
+import TaskItem from "./TaskItem";
+
+export default function TaskList() {
+  return (
+    <div className="border-b-[1px] border-gray-300 pb-[12px]">
+      <TaskItem />
+    </div>
+  );
+}
