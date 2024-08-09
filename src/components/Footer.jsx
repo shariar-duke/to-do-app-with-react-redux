e46de1f428567794432c890ec5aec60a72f8fd9b
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-09 13:04:41 +0600

    todo remaining

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,12 @@
-
+import { useSelector } from "react-redux";
 export default function Footer() {
+  const todos = useSelector((state) => state.todos);
+  // to do remaining , mane holo j todo gula complet hoy nai se gula nia aso 
+  const todoRemaining = todos.filter((todo)=> !todo.completed).length;
+
   return (
     <div className="flex justify-between mt-[10px]">
-      <p className="text-[12px] text-gray-600">2 tasks left</p>
+      <p className="text-[12px] text-gray-600">{todoRemaining} tasks left</p>
       <div className="flex gap-[4px]">
       <p className="text-[12px] text-gray-600">All</p>
       <p className="text-[12px] text-gray-600">|</p>
@@ -10,9 +14,9 @@ export default function Footer() {
       <p className="text-[12px] text-gray-600">|</p>
       <p className="text-[12px] text-gray-600">Complete</p>
       <div className="flex gap-[4px] items-center">
-        <div className="flex-shrink-0 h-3 w-3 rounded-full border-2 ml-auto cursor-pointer border-green-500 hover:bg-green-500"></div>
-        <div className="flex-shrink-0 h-3 w-3 rounded-full border-2 ml-auto cursor-pointer border-yellow-500 hover:bg-yellow-500"></div>
-        <div className="flex-shrink-0 h-3 w-3 rounded-full border-2 ml-auto cursor-pointer border-red-500 hover:bg-red-500"></div>
+        <div className={`flex-shrink-0 h-3 w-3 rounded-full border-2 ml-auto cursor-pointer border-green-500 hover:bg-green-500`}></div>
+        <div className={`flex-shrink-0 h-3 w-3 rounded-full border-2 ml-auto cursor-pointer border-yellow-500 hover:bg-yellow-500`}></div>
+        <div className={`flex-shrink-0 h-3 w-3 rounded-full border-2 ml-auto cursor-pointer border-red-500 hover:bg-red-500`}></div>
      
         </div>
       </div>
