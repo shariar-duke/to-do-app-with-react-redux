2c204019ae64afe2c3437289bcb6a7102d177e37
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-09 09:24:06 +0600

    reducer of filter slice

---
+++ src/redux/filters/reducer.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-case-declarations */
+import { COLORCHANGED, STATUSCHANGED } from "./actionTypes";
+import initialState from "./initialState";
+
+
+export  const reducer =(state = initialState , action) => 
+{
+    switch (action.type)
+    {
+      case STATUSCHANGED: 
+        
+         return {
+            ...state, 
+            status : action.paylod,
+         }
+
+     case COLORCHANGED : 
+        
+         const {color, changeType} = action.paylod;
+
+         switch(changeType)
+         {
+            case "added":
+                return {
+                    ...state, 
+                    color: [...state.colors, color]
+                }
+
+
+            case "remove":
+                return {
+                    ...state, 
+                    colors : state.colors.filter((existingColor)=> existingColor !== color)
+                }
+
+            default:
+                return state
+               
+
+         }
+
+    default:
+        return state     
+
+    }
+}
+
